6f3ac94424c4e0d4382918132337382e780a1e0e
Brian Jepson <brian.jepson@raspberrypi.com>
2025-05-14 11:38:43 -0400

    chapter 10 examples

---
+++ code/ch10/graph_temp.py
@@ -0,0 +1,13 @@
+from gpiozero import MCP3008, LEDBarGraph
+from time import sleep
+
+def convert_temp(gen):
+    for value in gen:
+        yield (value * 3.3 - 0.5) * 100
+
+adc = MCP3008(channel=7)
+graph = LEDBarGraph (26, 19, 13, 6, 5, pwm=True)
+for temp in convert_temp(adc.values):
+    bars = temp / 40
+    graph.value = bars
+    sleep(1)
