bd374aa1e830037606dfaf846302218ecd4f3086
Brian Jepson <bjepson@gmail.com>
2025-05-14 09:45:48 +0100

    simplified the code a bit

--- code/ch08/ldr_test.py
+++ code/ch08/ldr_test.py
@@ -4,27 +4,24 @@ from time import sleep, time
 def LDR_value(pin, charge_time_limit=0.005, min_readings=100):
     
     # Take the pin LOW to discharge the capacitor
-    ldr = OutputDevice(pin=pin, active_high=True, 
-                       initial_value=False)
+    ldr = OutputDevice(pin=pin)
+    ldr.off()
     sleep(0.1)
-
-    # Configure the pin as an input
     ldr.close()
-    ldr = InputDevice(pin=pin, pull_up=None,
-                      active_state=True)
 
-    # Wait for the capacitor to recharge, but only up to
-    # the charge time limit
+    # Configure the pin as a floating input
+    ldr = InputDevice(pin=pin, pull_up=None, active_state=True)
+
+    # Wait until the time limit for the capacitor to recharge 
     lit = 0
     start = time()
-    while True:
-        if time() - start >  charge_time_limit:
-            break
+    while time() - start < charge_time_limit:
         if ldr.is_active:
-            lit += 1
-
+            lit = 1
+            break
     ldr.close()
-    return lit > min_readings
+
+    return lit
 
 while True:
     print(LDR_value(4))
