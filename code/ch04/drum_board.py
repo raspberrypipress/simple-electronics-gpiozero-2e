309883b23eb31e858ebee0d970498b4bfea7488a
Brian Jepson <brian.jepson@raspberrypi.com>
2025-05-27 15:14:34 -0400

    why use os.path.join if I'm already using forwards slashes?

--- code/ch04/drum_board.py
+++ code/ch04/drum_board.py
@@ -2,7 +2,6 @@ from gpiozero import Button
 import pygame.mixer
 from pygame.mixer import Sound
 from signal import pause
-import os
 
 pygame.mixer.init()
 samples = "/usr/share/sonic-pi/samples/"
@@ -10,9 +9,11 @@ sound_pins = {
     2: "drum_bass_soft.flac",
     3: "drum_cowbell.flac",
 }
+
 buttons = [Button(pin) for pin in sound_pins]
 for button in buttons:
     file = sound_pins[button.pin.number]
-    sound = Sound(os.path.join(samples, file))
+    sound = Sound(f"{samples}/{file}")
     button.when_pressed = sound.play
+
 pause()
--- code/ch04/drumbeat.py
+++ code/ch04/drumbeat.py
@@ -1,9 +1,8 @@
 import pygame.mixer
 from pygame.mixer import Sound
-import os
 
 pygame.mixer.init()
 samples = "/usr/share/sonic-pi/samples/"
-drum = Sound(os.path.join(samples, "drum_bass_soft.flac"))
+drum = Sound(f"{samples}/drum_bass_soft.flac")
 while True:
     drum.play()
